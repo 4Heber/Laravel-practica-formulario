786ed0400f4ac221c49d90be9737487922dca7b9
4Heber <parzival.html@gmail.com>
2023-01-23 15:13:30 +0100

    Vistas actualizadas

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,21 +2,40 @@
     <x-slot name="title">Posts</x-slot>
 
     <x-slot name="header">
-        <div class="container-fluid">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Posts</h2>
-        </div>
+        <header class="p-3 text-bg-dark">
+            <div class="container">
+                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                        <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
+                    </a>
+
+                    <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                        <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+                        <li><a href="#" class="nav-link px-2 text-white">Posts</a></li>
+                        <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+
+                        <!-- route('posts.create') -->
+                        <a href="#" class="text-sm text-gray-700 dark:text-gray-500 ms-5 btn btn-outline-light">Create new post</a>
+                    </ul>
+
+                    <div class="text-end">
+                        @if (Route::has('login'))
+                            @auth
+                                <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                            @else
+                                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline btn btn-outline-light me-2">Login</a>
+
+                            @if (Route::has('register'))
+                                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline btn btn-warning">Sign-up</a>
+                                @endif
+                            @endauth
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </header>
     </x-slot>
 
-    <ul class="nav justify-content-center">
-        <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Active</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-    </ul>
+
 
 </x-app-layout>
